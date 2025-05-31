ec6a7cb2f916ffddb6600f225ce2796fed85ac96
PaulaLinhares <paulalingares@gmail.com>
2025-05-31 08:42:15 -0300

    Create calculadoranotas.py
    Primeiro arquivo do projeto, calcula a média entre duas notas com função em Python.

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+n1=float(input("Digite a primeira nota:"))
+n2=float(input("Digite a segunda nota:"))
+média=(n1+n2)/2
+print("A sua média entre {} e {} é igual a {:.1f}".format(n1,n2,média))
+#Calculando notas de alunos
